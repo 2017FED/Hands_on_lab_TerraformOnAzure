ce3d22b454284b9f981aa0254ed669e123c20d7a
2017FED <sainiparminder2015@gmail.com>
2023-05-07 13:48:24 +1000

    Adding infrastructure

---
+++ main.tf
@@ -0,0 +1,36 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.43.0"
+    }
+  }
+  cloud {
+    organization = "ParminderSaini"
+
+    workspaces {
+      name = "Hands_on_lab_TerraformOnAzure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-684ec07f-hands-on-with-terraform-on-azure"
+  location = "South Central US"
+}
+
+module "securestorage2" {
+  source               = "app.terraform.io/ParminderSaini/securestorage2/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "strFinallab1"
+}
